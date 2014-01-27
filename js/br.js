91bdf5db739b7957312f141de6b8c89e180a1922
Blaine Fricks <blaine.fricks@live.com>
2014-01-27 11:26:24 -0800

    v0.2.18
    Updated menu icon.

--- js/br.js
+++ js/br.js
@@ -1,1 +1,9 @@
-// empty javascript file
+function clickHandler(event) {
+  var eType = event.type;
+  /* the following is for compatibility */
+  /* Moz populates the target property of the event object */
+  /* IE populates the srcElement property */
+  var eTarget = event.target || event.srcElement;
+
+  alert( "Captured Event (type=" + eType + ", target=" + eTarget );
+}
