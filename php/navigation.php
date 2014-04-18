6693081a48aee8372bfe69d606600e456a504f65
Blaine Fricks <blaine.fricks@live.com>
2014-04-18 08:50:54 -0700

    v0.2.51

---
+++ php/navigation.php
@@ -0,0 +1,16 @@
+<?php echo <<< EOT ?>
+<div class="sliding-nav">
+  <ul class="primary-nav">
+    <li><a href="index.html">Home</a></li>
+    <li><a href="ui-library.html">UI Library</a></li>
+  </ul>
+</div>
+<div class="tablet-nav">
+  <div class="line selfclear">
+    <div>
+      <a href="#"><span class="menuIcon"></span></a>
+      <a href="index.html">Blaine Report</a>
+    </div>
+  </div>
+</div>;
+<?php EOT; ?>
