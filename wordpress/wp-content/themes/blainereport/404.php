8d84726b21395cf82fb176020e466e1c9371d895
Blaine Fricks <blaine.fricks@live.com>
2014-04-28 21:18:19 -0700

    v0.2.60

---
+++ wordpress/wp-content/themes/blainereport/404.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+<div class="main-content">
+  <div class="line main-section">
+    <div class="lastUnit size1of1 sub-section">
+    <h2>Recent Stories</h2>
+    <p>test</p>
+    <ol>
+      <li><a href="story.php">The Thing (2011)</a></li>
+    </ol>
+    </div>
+  </div>
+</div>
+<?php get_footer(); ?>
---
+++ wordpress/wp-content/themes/blainereport/versions.php
@@ -0,0 +1,34 @@
+<?php get_header(); ?>
+<div class="main-content">
+  <div class="line main-section">
+    <div class="unit size1of1 header">
+      <h1>Blaine Report v<span class="current-version"></span></h1>
+    </div>
+    <div class="lastUnit size1of1">
+      <div class="sub-section below60">
+        <p>Because why the heck not? You clicked on the little number at the bottom right of the page, so here is your reward.</p>
+      </div>
+      <div class="sub-section">
+        <h3>Numbering System</h3>
+        <p><i>version.update.revision</i></p>
+        <p><b>Version</b> - Usually signifying a large change to the code or launch of new feature set.</p>
+        <p><b>Update</b> - Current update that has been applied to the current version.</p>
+        <p><b>Revision</b> - Number of changes to the current update.</p>
+      </div>
+    </div>
+  </div>
+  <div class="line main-section">
+    <div class="lastUnit size1of1">
+      <div class="sub-section">
+        <h2>Version History</h2>
+        <h4>v1.0</h4>
+        <p>v1.0 - In Development</p>
+      </div>
+    </div>
+  </div>
+  <div class="line main-section">
+    <div class="lastUnit size1of1">
+    </div>
+  </div>
+</div>
+<?php get_footer(); ?>
