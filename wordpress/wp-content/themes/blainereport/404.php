4de37787cf24762cca060bde0e7af2ea6fb964cd
Blaine Fricks <blaine.fricks@live.com>
2014-05-01 08:25:21 -0700

    v0.2.62
    Set up 404 and made the page height a minimum of 600px or all pages so
    that the footer is near the bottom on most screens.

--- wordpress/wp-content/themes/blainereport/404.php
+++ wordpress/wp-content/themes/blainereport/404.php
@@ -2,11 +2,8 @@
 <div class="main-content">
   <div class="line main-section">
     <div class="lastUnit size1of1 sub-section">
-    <h2>Recent Stories</h2>
-    <p>test</p>
-    <ol>
-      <li><a href="story.php">The Thing (2011)</a></li>
-    </ol>
+      <h2>Error 404</h2>
+      <p>Need some funny text to inform the user that the page was not found.</p>
     </div>
   </div>
 </div>
--- wordpress/wp-content/themes/blainereport/index.php
+++ wordpress/wp-content/themes/blainereport/index.php
@@ -23,7 +23,6 @@ get_header(); ?>
   <div class="line main-section">
     <div class="lastUnit size1of1 sub-section">
     <h2>Recent Stories</h2>
-    <p>test</p>
     <ol>
       <li><a href="story.php">The Thing (2011)</a></li>
     </ol>
